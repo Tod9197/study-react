b083562b662800acf39cb214f144afcf172f7c45
Tod9197 <dancein18@yahoo.co.jp>
2024-05-08 17:19:25 +0900

    ⚗️ クリックイベントの実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,19 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "../components/Main/Main";
 import { Header } from "../components/Header/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <>
       <Head>
@@ -13,6 +22,9 @@ export default function Home() {
       </Head>
       <Header />
       <Main page="index" />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
     </>
   );
 }
